ccbf57b3a89fca2c2dc97b9223985b0fa5c2b75f
Kaustubh Trivedi <2k23.csaiml2311171@gmail.com>
2026-03-04 03:19:21 +0530

    1st

---
+++ max_consecutive_ones.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+class Solution {
+    public:
+        int findMaxConsecutiveOnes(vector<int>& nums) {
+            int count=0;
+             int maxi=0;
+            for (int i=0;i<nums.size();i++)
+            {
+                if (nums[i]==1)
+                count++;
+                else
+                maxi=max(max,count);
+                count =0;
+            }
+            return maxi;
+        }
+    };
