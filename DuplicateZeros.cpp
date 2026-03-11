7eb33bc5d863f7b422aa93aab42d560cb191ef1d
Kaustubh Trivedi <2k23.csaiml2311171@gmail.com>
2026-03-11 17:48:41 +0530

    leetcode4th

---
+++ DuplicateZeros.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    public:
+        void duplicateZeros(vector<int>& arr) {
+    
+            vector<int> temp;
+    
+            for(int i = 0; i < arr.size(); i++)
+            {
+                if(arr[i] == 0)
+                {
+                    temp.push_back(0);
+                    temp.push_back(0);
+                }
+                else
+                {
+                    temp.push_back(arr[i]);
+                }
+    
+                if(temp.size() >= arr.size())
+                    break;
+            }
+    
+            for(int i = 0; i < arr.size(); i++)
+            {
+                arr[i] = temp[i];
+            }
+        }
+    };
